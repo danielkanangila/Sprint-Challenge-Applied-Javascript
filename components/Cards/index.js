d25cde7f1626498fc3c506b6d736273ce869e2db
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-12-13 19:23:58 -0500

    Added Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,45 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+(function() {
+    const html = String.raw;
+    const base_url = 'https://lambda-times-backend.herokuapp.com/articles';
+    const parser = new DOMParser();
+
+    async function fetchData() {
+        try {
+            return await axios.get(base_url);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    function Card({topic, headline, authorPhoto, authorName}) {
+        const template = html`
+            <div class="card" data-tab-content="${topic}">
+               <div class="headline">${headline}</div>
+               <div class="author">
+                 <div class="img-container">
+                   <img src=${authorPhoto} />
+                 </div>
+                 <span>By ${authorName}</span>
+               </div>
+            </div>
+        `
+
+        return parser.parseFromString(template, 'text/html')
+            .body.firstChild;
+    }
+
+    async function addCardToDOM() {
+        const articles = await fetchData();
+        const articlesContainer = document.querySelector('.cards-container');
+        for (let [key, value] of Object.entries(articles.data.articles)) {
+            value.forEach(
+                article => articlesContainer.appendChild(Card({topic: key, ...article}))
+            );
+        }
+    }
+
+    addCardToDOM();
+})();
